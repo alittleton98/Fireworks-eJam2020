d57a383c1c6817801507e2b38d79c44320cab0f5
David Gubala <gubdev@protonmail.com>
2020-06-27 01:18:57 -0500

    Simple Character Movement Unity Demo

---
+++ Fireworks-eJam/Assets/Demo/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 6f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(h, 0f, v).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+    }
+}
